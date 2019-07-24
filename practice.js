c059b49e5f9933e18793b835b1890613f46c1284
McKay Nelson <tmckaynelson@gmail.com>
2019-07-24 14:55:31 -0600

    all completed necessary, some black diamond

--- practice.js
+++ practice.js
@@ -19,7 +19,9 @@ var arr = [10,20,30];
 
 //Code Here
 
-
+function first(arr) {
+  return arr[0]
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[arr.length-1]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family) {
+  for (let i = 0; i < family.length; i++){
+    alert(family[i])
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters) {
+  for (let i = letters.length - 1; i > -1; i--) {
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +90,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder(nums) {
+  let evens = []
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      evens.push(nums[i])
+    }
+  }
+  return evens
+}
 
 
 
@@ -107,7 +127,16 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
+function divider(arr) {
+  let evens = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evens.push(arr.splice(i,1))
+    }
+  }
 
+  return [evens, arr]
+}
 
 
 ////////// PROBLEM 7 //////////
@@ -129,7 +158,16 @@ var getRandomArbitrary = function() {
 */
 
 //Code Here
+function finder(anArray) {
+  let rand = getRandomArbitrary
 
+  for (let i = 0; i < anArray.length; i++) {
+    if (anArray[i] === rand) {
+      return true
+    }
+  }
+  return false
+}
 
 
 ////////// PROBLEM 8 //////////
